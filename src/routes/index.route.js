7e90f95ff58f8e2e4fcce9346c1b2618c6d55d27
juan <juanhotz611@gmail.com>
2022-02-01 17:42:57 -0300

    FIX: remove console.log

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,7 +3,6 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('', (req, res) => {
-  console.log(__dirname);
   res.render(path.join(__dirname, '../public/index.ejs'));
 });
 
