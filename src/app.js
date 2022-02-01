a1aae6a942c88d59dccc12aa4534c970665db718
juan <juanhotz611@gmail.com>
2022-02-01 13:01:39 -0300

    Fix: app.use route

--- src/app.js
+++ src/app.js
@@ -8,6 +8,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(morgan('dev'));
-app.use('', routes);
+app.use('/', routes);
 
 module.exports = app;
