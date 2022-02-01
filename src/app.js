cb07f0d4257d08737d1bfa09f7a95ba49bb18931
juan <juanhotz611@gmail.com>
2022-02-01 13:01:09 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const routes = require('./video');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(morgan('dev'));
+app.use('', routes);
+
+module.exports = app;
