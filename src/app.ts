d06f6dbd92631165f7de1cb34a92683a77ca631e
juan <juanhotz611@gmail.com>
2022-02-03 17:14:23 -0300

    REFACTOR: moved express.static

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,8 @@ const app: Application = express();
 
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.json({ limit: '25mb' }));
-app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(morgan('dev'));
 app.use(setHeaders);
 
 app.use('/', routes);
