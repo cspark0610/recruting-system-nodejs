9c32e1d50da9c5aeef4e5ac43b72af580cfef85d
juan <juanhotz611@gmail.com>
2022-02-02 23:32:34 -0300

    FEAT: add setHeaders middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const morgan = require('morgan');
 
 const routes = require('./routes/index.route');
+const { setHeaders } = require('./middlewares/setHeaders');
 
 const app = express();
 
@@ -10,16 +11,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(setHeaders);
 
 app.use('/', routes);
 
