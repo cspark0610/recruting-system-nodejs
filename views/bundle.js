1aa24c14cd8de88fef2cd3a5618224f613e87ce0
juan <juanhotz611@gmail.com>
2022-02-04 15:01:42 -0300

    FIX: bundle.js not modified

--- views/bundle.js
+++ views/bundle.js
@@ -2105,99 +2105,110 @@ window.onload = () => {
 
   watchContainer.style.display = 'none';
 
+  const setElementsDisplay = (
+    startReocordingButtonDisplay = 'block',
+    stopRecordingButtonDisplay = 'none',
+    finishRecordingButtonDisplay = 'none',
+    reRecordButtonDisplay = 'none',
+    watchContainerDisplay = 'none',
+  ) => {
+    startReocordingButton.style.display = startReocordingButtonDisplay;
+    stopRecordingButton.style.display = stopRecordingButtonDisplay;
+    finishRecordingButton.style.display = finishRecordingButtonDisplay;
+    reRecordButton.style.display = reRecordButtonDisplay;
+    watchContainer.style.display = watchContainerDisplay;
+  };
+
+  const startTimer = () => {
+    watchSeconds++;
+
+    if (watchSeconds <= 9) {
+      watchSecondsElement.innerHTML = `0${watchSeconds}`;
+    }
+
+    if (watchSeconds > 9) {
+      watchSecondsElement.innerHTML = watchSeconds;
+    }
+
+    if (watchSeconds > 60) {
+      watchMinutes++;
+      watchMinutesElement.innerHTML = `0${watchMinutes}`;
+
+      watchSeconds = 0;
+      watchSecondsElement.innerHTML = '0' + 0;
+    }
+
+    if (watchMinutes > 9) {
+      watchMinutesElement.innerHTML = watchMinutes;
+    }
+  };
+
+  const startRecording = () => {
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start(10);
+    mediaRecorder.ondataavailable = (e) => {
+      videoChunks.push(e.data);
+    };
+
+    interval = setInterval(startTimer, 1000);
+
+    setElementsDisplay('none', 'block', 'none', 'none', 'block');
+  };
+
+  const stopRecording = () => {
+    mediaRecorder.stop();
+    clearInterval(interval);
+
+    setElementsDisplay('none', 'none', 'block', 'block');
+  };
+
+  const finishRecordingAndSendVideo = async () => {
+    try {
+      const blob = new Blob(videoChunks, {
+        type: 'video/mp4',
+      });
+
+      const formData = new FormData();
+      formData.append('video', blob);
+
+      await axios.post('/video/save', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+
+      videoChunks.length = 0;
+
+      setElementsDisplay('none', 'none', 'none', 'block', 'none');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   navigator.mediaDevices
     .getUserMedia({ audio: true, video: true })
     .then((stream) => {
       document.getElementById('video').srcObject = stream;
 
-      startReocordingButton.onclick = () => {
-        mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.start(10);
-        mediaRecorder.ondataavailable = (e) => {
-          videoChunks.push(e.data);
-        };
+      startReocordingButton.onclick = () => startRecording();
 
-        watchContainer.style.display = 'block';
+      stopRecordingButton.onclick = () => stopRecording();
 
-        interval = setInterval(startTimer, 1000);
-
-        stopRecordingButton.style.display = 'block';
-        startReocordingButton.style.display = 'none';
-      };
-
-      stopRecordingButton.onclick = () => {
-        mediaRecorder.stop();
-        clearInterval(interval);
-        stopRecordingButton.style.display = 'none';
-        finishRecordingButton.style.display = 'block';
-        reRecordButton.style.display = 'block';
-      };
-
-      finishRecordingButton.onclick = async () => {
-        try {
-          const blob = new Blob(videoChunks, {
-            type: 'video/mp4',
-          });
-
-          const formData = new FormData();
-          formData.append('video', blob);
-
-          await axios.post('/video/save', formData, {
-            headers: { 'Content-Type': 'multipart/form-data' },
-          });
-
-          videoChunks.length = 0;
-          finishRecordingButton.style.display = 'none';
-        } catch (e) {
-          console.error(e);
-        }
-      };
+      finishRecordingButton.onclick = async () =>
+        await finishRecordingAndSendVideo();
 
       reRecordButton.onclick = () => {
         alert('ATENCION!! Perderas el video anterior...');
 
-        stopRecordingButton.style.display = 'block';
-        finishRecordingButton.style.display = 'none';
-        reRecordButton.style.display = 'none';
+        setElementsDisplay('none', 'block', 'none', 'none', 'block');
+
         videoChunks.length = 0;
 
-        mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.start(10);
-        mediaRecorder.ondataavailable = (e) => {
-          videoChunks.push(e.data);
-        };
-
-        interval = setInterval(startTimer, 1000);
+        startRecording();
 
         watchMinutes = '00';
         watchSeconds = '00';
 
         watchMinutesElement.innerHTML = watchMinutes;
         watchSecondsElement.innerHTML = watchSeconds;
-      };
-
-      const startTimer = () => {
-        watchSeconds++;
-
-        if (watchSeconds <= 9) {
-          watchSecondsElement.innerHTML = `0${watchSeconds}`;
-        }
-
-        if (watchSeconds > 9) {
-          watchSecondsElement.innerHTML = watchSeconds;
-        }
-
-        if (watchSeconds > 60) {
-          watchMinutes++;
-          watchMinutesElement.innerHTML = `0${watchMinutes}`;
-
-          watchSeconds = 0;
-          watchSecondsElement.innerHTML = '0' + 0;
-        }
-
-        if (watchMinutes > 9) {
-          watchMinutesElement.innerHTML = watchMinutes;
-        }
       };
     })
     .catch((e) => console.error(e));
