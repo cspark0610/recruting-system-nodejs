d0bd4e2b38a54da5c2bce60d38ea16e53cdf9dcf
juan <juanhotz611@gmail.com>
2022-02-03 22:15:45 -0300

    FIX: app port

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import app from './src/app';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { APP_PORT } = process.env || 3001;
+const PORT: number | string = process.env.PORT || 3001;
 
-app.listen(APP_PORT, () => console.log(`app running on PORT ${APP_PORT}`));
+app.listen(PORT, () => console.log(`app running on PORT ${PORT}`));
