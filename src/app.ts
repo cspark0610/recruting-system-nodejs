f094f6c6124cec724d8c78f5bbac7f5bff49e3d7
juan <juanhotz611@gmail.com>
2022-02-03 22:05:24 -0300

    FEAT: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,10 @@ import { Application } from 'express';
 import express from 'express';
 import path from 'path';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import routes from './routes/index.route';
-import setHeaders from './middlewares/setHeaders';
+//import setHeaders from './middlewares/setHeaders';
 
 const app: Application = express();
 
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.json({ limit: '25mb' }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(morgan('dev'));
-app.use(setHeaders);
+app.use(cors);
 
 app.use('/', routes);
 
