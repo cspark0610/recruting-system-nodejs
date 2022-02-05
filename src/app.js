1d95fc6e846b05393c8a3386e18520762f78d661
juan <juanhotz611@gmail.com>
2022-02-05 12:29:46 -0300

    FEAT: add NODE_ENV validation in app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
-
-const routes = require('./routes/index.route');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+}
+
+const routes = require('./routes/index.route');
 
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.json({ limit: '25mb' }));
 app.use(express.static(path.join(__dirname, '../views')));
-app.use(morgan('dev'));
 
 app.set('view engine', 'ejs');
 
