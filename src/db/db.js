4abdaad10ead686dfdd36e7638d8192c7ed57be9
juan <juanhotz611@gmail.com>
2022-02-05 16:35:20 -0300

    FIX: mongodb uris

--- src/db/db.js
+++ src/db/db.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 if (process.env.NODE_ENV === 'development') {
-  mongoose.connect(process.env.MONGODB_URL_DEVELOPMENT, {
+  mongoose.connect(process.env.MONGODB_DEVELOPMENT_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 } else if (process.env.NODE_ENV === 'production') {
-  mongoose.connect(process.env.MONGODB_URL_PRODUCTION, {
+  mongoose.connect(process.env.MONGODB_PRODUCTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
