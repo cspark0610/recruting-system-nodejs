9158130cb2248409c0e55fdbd91f621dd08a6ee1
juan <juanhotz611@gmail.com>
2022-02-05 19:17:18 -0300

    FIX: mongoose.connect

--- src/db/db.js
+++ src/db/db.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 if (process.env.NODE_ENV === 'development') {
-  mongoose.connect(process.env.MONGODB_DEVELOPMENT_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(process.env.MONGODB_DEVELOPMENT_URI, (error) =>
+    error ? console.error(error) : null,
+  );
 } else if (process.env.NODE_ENV === 'production') {
-  mongoose.connect(process.env.MONGODB_PRODUCTION_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(process.env.MONGODB_PRODUCTION_URI, (error) =>
+    error ? console.error(error) : null,
+  );
 }
