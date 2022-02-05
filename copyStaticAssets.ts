db0df891ffa7b872837e00a046de69496ca09f44
juan <juanhotz611@gmail.com>
2022-02-05 20:47:47 -0300

    FIX: videoProcessing in prod not found

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,7 +1,6 @@
 import * as shelljs from 'shelljs';
 
-shelljs.cp('-R', 'public/js', 'dist/public');
-shelljs.cp('-R', 'public/css', 'dist/public');
+shelljs.cp('-R', 'public', 'dist');
 shelljs.cp('-R', 'views', 'dist');
 shelljs.cp('-R', 'uploads', 'dist');
 shelljs.cp('-R', 'downloads', 'dist');
