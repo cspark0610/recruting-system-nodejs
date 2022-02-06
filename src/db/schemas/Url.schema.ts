0c083d1212186d5d4eb51f54b56cc9f7d2cd25cb
juan <juanhotz611@gmail.com>
2022-02-06 15:29:07 -0300

    FEAT: remove versionKey from schema

--- src/db/schemas/Url.schema.ts
+++ src/db/schemas/Url.schema.ts
@@ -1,24 +1,27 @@
 import mongoose from 'mongoose';
 import shortId from 'shortid';
 
-const UrlSchema = new mongoose.Schema({
-  redirect_url: {
-    type: String,
-    required: true,
-  },
+const UrlSchema = new mongoose.Schema(
+  {
+    redirect_url: {
+      type: String,
+      required: true,
+    },
 
-  short_url: {
-    type: String,
-    required: true,
-    default: shortId.generate,
-  },
+    short_url: {
+      type: String,
+      required: true,
+      default: shortId.generate,
+    },
 
-  expiresAt: {
-    type: Date,
-    default: new Date(),
-    index: { expires: '24h' },
+    expiresAt: {
+      type: Date,
+      default: new Date(),
+      index: { expires: '24h' },
+    },
   },
-});
+  { versionKey: false },
+);
 
 UrlSchema.index({ expiresAt: 1 }, { expires: '24h' });
 
