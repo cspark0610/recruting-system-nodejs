0a3b21df4940bb2a216dbc2a3bf1e25911d86ebd
juan <juanhotz611@gmail.com>
2022-02-14 12:19:09 -0300

    FEAT: root url now is not valid

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -19,7 +19,7 @@ export const validateUrl = async (req: Request, res: Response) => {
       return res.status(404).redirect('validate/error/not-valid');
     }
 
-    res.redirect(uniqueUrlId.redirect_url);
+    res.render('pages/index')
   } catch (e) {
     return res.send(e);
   }
