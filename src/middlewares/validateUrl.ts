75ac8f4ac7967b23b5e9a82f63dae25cd4e41845
juan <juanhotz611@gmail.com>
2022-02-15 11:57:36 -0300

    FIX: linter

--- src/middlewares/validateUrl.ts
+++ src/middlewares/validateUrl.ts
@@ -1,17 +1,21 @@
-import { Request, Response, NextFunction } from 'express'
-import Url from '../db/schemas/Url.schema'
+import { Request, Response, NextFunction } from 'express';
+import Url from '../db/schemas/Url.schema';
 
-export default async function validateUrl(req: Request, res: Response, next: NextFunction) {
+export default async function validateUrl(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   try {
-    const id = req.query.id as string
+    const id = req.query.id as string;
 
-    const uniqueUrl = await Url.findOne({ short_url: id })
+    const uniqueUrl = await Url.findOne({ short_url: id });
 
     if (!uniqueUrl || Object.entries(uniqueUrl).length === 0) {
-      return res.status(404).render('pages/urlNotValid')
+      return res.status(404).render('pages/urlNotValid');
     }
-    next()
+    next();
   } catch (e: any) {
-    return new Error(e)
+    return new Error(e);
   }
 }
