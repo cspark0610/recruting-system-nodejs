339f12e27acf86deb4174fd6ca0a79b925e1e785
juan <juanhotz611@gmail.com>
2022-02-17 17:48:03 -0300

    FEAT: add validateUserId middleware

---
+++ src/middlewares/validateUserId.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import User from '../db/schemas/User.schema';
+import IUser from '../interfaces/IUser.interface';
+import temp from '../lib/tempVariables';
+
+const validateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { userId } = req.params;
+
+    const user: IUser | null = await User.findOne({ id: userId });
+
+    if (!user || Object.entries(user).length === 0) {
+      return res.status(404).send({
+        status: 'failure',
+        code: 404,
+        message: `user not found with id ${userId}`,
+      });
+    }
+
+    if (!user.video_key) {
+      return res.status(400).send({
+        status: 'failure',
+        code: 400,
+        message: 'User is found, but has yet to record their video',
+      });
+    }
+
+    temp.video_key = user.video_key;
+
+    next();
+  } catch (e: any) {
+    console.error(e);
+  }
+};
+
+export default validateUser;
