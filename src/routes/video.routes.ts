79c52a7ef32715238bf2cc8771e00fb8b6c95a94
juan <juanhotz611@gmail.com>
2022-02-17 17:48:43 -0300

    FEAT: add video get route that receives the user id

--- src/routes/video.routes.ts
+++ src/routes/video.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import storage from '../lib/multerConfig';
+import validateUser from '../middlewares/validateUserId';
 import * as videoController from '../controllers/video.controller';
 
 const upload = multer({ storage });
@@ -8,6 +9,8 @@ const upload = multer({ storage });
 const router = Router();
 
 router.get('/get/:key', videoController.getVideoFromS3);
+
+router.get('/view/:userId', validateUser, videoController.getVideo);
 
 router.post('/upload', upload.single('video'), videoController.uploadVideoToS3);
 
