4af9dd24d07b1be937fcf393b9612b8b54aaaafd
juan <juanhotz611@gmail.com>
2022-02-21 10:40:53 -0300

    FEAT: add unique attribute to schemas. Remove unnecessary user id validation

--- src/db/schemas/User.schema.ts
+++ src/db/schemas/User.schema.ts
@@ -6,17 +6,20 @@ const UserSchema = new Schema(
     id: {
       type: String,
       required: true,
+      unique: true,
     },
 
     video_key: {
       type: String,
       required: false,
+      unique: true,
     },
 
     index: {
       type: String,
       required: true,
       default: generate,
+      unique: true,
     },
   },
   { versionKey: false },
