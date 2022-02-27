c3dc41b0a3d0a059e9515931dbde110b297b2b25
Nacho Hotz <juanhotz611@gmail.com>
2022-02-27 18:04:00 -0300

    FIX: eslint

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,7 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 if (process.env.NODE_ENV === 'development') {
-  mongoose.connect(process.env.MONGODB_DEVELOPMENT_URI as string, (error) => (error ? console.error(error) : console.log('database connected')));
+  mongoose.connect(process.env.MONGODB_DEVELOPMENT_URI as string, (error) =>
+    error ? console.error(error) : console.log('database connected'),
+  );
 } else if (process.env.NODE_ENV === 'production') {
-  mongoose.connect(process.env.MONGODB_PRODUCTION_URI as string, (error) => (error ? console.error(error) : console.log('database connected')));
+  mongoose.connect(process.env.MONGODB_PRODUCTION_URI as string, (error) =>
+    error ? console.error(error) : console.log('database connected'),
+  );
 }
