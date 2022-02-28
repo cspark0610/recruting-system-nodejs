f9bc6e059bc865b53f6f422a4d4a00e1e1c47f3d
Nacho Hotz <juanhotz611@gmail.com>
2022-02-28 13:14:19 -0300

    FEAT: now video viewer can render all canddidate videos

--- public/js/videoReceiver.js
+++ public/js/videoReceiver.js
@@ -1,22 +1,30 @@
 window.onload = async () => {
-  const loadingText = document.getElementById('loading');
   try {
-    const videoKeys = await fetch('/video-key');
-    const videoKeysParsed = await videoKeys.json();
+    const keys = await fetch('/video-key');
+    let videoKeysParsed = await keys.json();
+    videoKeysParsed = videoKeysParsed.video_data;
 
-    const video = await fetch(`/video/get/${videoKeysParsed.index}`, {
-      method: 'get',
+    let videosFile = videoKeysParsed.map((video) =>
+      fetch(`/video/get/${video.video_key}`).then((res) => res.blob()),
+    );
+
+    videosFile = await Promise.all(videosFile);
+    videosFile = videosFile.map((blob) => window.URL.createObjectURL(blob));
+
+    const mainWrapper = document.querySelector('.video-viewer');
+
+    videosFile.forEach((video, index) => {
+      const videoElement = document.createElement('video');
+      const question = document.createElement('h4');
+
+      question.innerHTML = `Pregunta ${videoKeysParsed[index].question_id} - ${videoKeysParsed[index].question_title}`;
+
+      videoElement.src = video;
+      videoElement.controls = true;
+
+      mainWrapper.appendChild(question);
+      mainWrapper.appendChild(videoElement);
     });
-
-    const videoElement = document.getElementById('video-container');
-
-    const blob = await video.blob();
-
-    const videoUrl = window.URL.createObjectURL(blob);
-
-    videoElement.src = videoUrl;
-    videoElement.controls = true;
-    loadingText.style.display = 'none';
   } catch (e) {
     console.error(e.message);
   }
