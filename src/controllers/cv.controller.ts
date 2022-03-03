6becac62898acb2674f7c6420adaf5324546a17b
Nacho Hotz <juanhotz611@gmail.com>
2022-03-03 12:05:36 -0300

    FEAT: create new AWS S3 bucket for cv. Create and finish cv controller and service for uplaod and download

---
+++ src/controllers/cv.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+import { unlink } from 'fs';
+import { promisify } from 'util';
+import UploadCV, { GetCV } from '../services/CV.service';
+
+const unlinkFile = promisify(unlink);
+
+export const getCV = async (req: Request, res: Response) => {
+  const { key } = req.params;
+
+  try {
+    const candidateCV = await GetCV(key);
+
+    if (!candidateCV) {
+      return res.status(404).send('No cv found');
+    }
+
+    candidateCV.pipe(res);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const uploadCV = async (req: Request, res: Response) => {
+  const cv = req.file;
+
+  if (!cv) {
+    return res.status(400).send({
+      status: 'failure',
+      code: 400,
+      message: 'No cv was received',
+    });
+  }
+
+  if (cv.mimetype !== 'application/pdf') {
+    await unlinkFile(cv.path);
+    return res.status(400).send('Only PDF files are supported');
+  }
+
+  try {
+    await UploadCV(cv);
+  } catch (e) {
+    console.error(e);
+  }
+
+  return res.status(201).send('File uploaded successfully');
+};
---
+++ src/services/CV.service.ts
@@ -0,0 +1,50 @@
+import S3 from 'aws-sdk/clients/s3';
+import dotenv from 'dotenv';
+import { createReadStream } from 'fs';
+import UploadParams from '../interfaces/UploadParams.interface';
+import File from '../interfaces/File.interface';
+
+dotenv.config();
+
+const {
+  AWS_BUCKET_REGION,
+  AWS_BUCKET_ACCESS_KEY,
+  AWS_BUCKET_SECRET_ACCESS_KEY,
+} = process.env;
+
+const s3 = new S3({
+  accessKeyId: AWS_BUCKET_ACCESS_KEY,
+  secretAccessKey: AWS_BUCKET_SECRET_ACCESS_KEY,
+  region: AWS_BUCKET_REGION,
+});
+
+export const GetCV = async (key: string) => {
+  try {
+    const getParams = {
+      Bucket: 'candidatescvbucket',
+      Key: key,
+    };
+
+    return s3.getObject(getParams).createReadStream();
+  } catch (e: any) {
+    console.error(e.message);
+  }
+};
+
+const UploadCV = async (cv: File) => {
+  try {
+    const fileStream = createReadStream(cv.path);
+
+    const uploadParams: UploadParams = {
+      Bucket: 'candidatescvbucket',
+      Body: fileStream,
+      Key: cv.filename,
+    };
+
+    return s3.upload(uploadParams).promise();
+  } catch (e: any) {
+    console.error(e.message);
+  }
+};
+
+export default UploadCV;
