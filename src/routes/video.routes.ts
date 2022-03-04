d5d0e12874e0d83917fd6008ad6b5ad50b9d2376
Nacho Hotz <juanhotz611@gmail.com>
2022-03-04 12:58:04 -0300

    FIX: router linter

--- src/routes/video.routes.ts
+++ src/routes/video.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import multer from 'multer';
 import storage from '../lib/multerConfig';
-import validateUser from '../middlewares/validateUserId';
+
 import * as videoController from '../controllers/video.controller';
+
+import validateUser from '../middlewares/validateUserId';
 
 const upload = multer({ storage });
 
 const router = Router();
 
 router.get('/get/:key', videoController.getVideoFromS3);
-
 router.get('/view/:userId', validateUser, videoController.getVideo);
 
 router.post(
