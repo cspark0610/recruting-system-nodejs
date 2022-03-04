145e7cba2830fe4223d972d9d54a5376c00df3d2
Nacho Hotz <juanhotz611@gmail.com>
2022-03-04 12:57:37 -0300

    FIX: router linter

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import * as urlController from '../controllers/url.controller';
+
 import validateUrl from '../middlewares/validateUrl';
 import validateUrlCreationParams from '../middlewares/validateUrlCreationParams';
 import validateUrlDeletion from '../middlewares/validateUrlDeletion';
@@ -8,9 +9,7 @@ import validateUrlDeletion from '../middlewares/validateUrlDeletion';
 const router = Router();
 
 router.get('/validate', validateUrl, urlController.renderApp);
-
 router.get('/validate/error/not-valid', urlController.renderNotValidUrl);
-
 router.get('/create', validateUrlCreationParams, urlController.generateUrl);
 
 router.delete('/delete/:url_id', validateUrlDeletion, urlController.deleteUrl);
