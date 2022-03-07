2c4abf806a6e718b93896a8716e9ad24f57ff0eb
Nacho Hotz <juanhotz611@gmail.com>
2022-03-07 17:02:39 -0300

    FEAT: add interface type into schemas initializations. Remove _id from interfaces

--- src/interfaces/ICandidate.interface.ts
+++ src/interfaces/ICandidate.interface.ts
@@ -1,11 +1,9 @@
 export default interface ICandidate {
-  _id: string;
   id: string;
   videos_question_list: Array<{
     question_id: number;
     question_title: string;
     video_key: string;
-    _id: string;
   }>;
   cv: string;
 }
--- src/interfaces/IUrl.interface.ts
+++ src/interfaces/IUrl.interface.ts
@@ -1,5 +1,4 @@
 export default interface IUrl {
-  _id: string;
   short_url: string;
   expiresAt: Date;
 }
