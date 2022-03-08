57180d6b2b0d3dc0547bbf3601b17126ee21a5c1
Nacho Hotz <juanhotz611@gmail.com>
2022-03-08 14:39:01 -0300

    FEAT: now uploaded cvs are deleted from fs

--- src/controllers/candidate.controller.ts
+++ src/controllers/candidate.controller.ts
@@ -1,8 +1,12 @@
 /* eslint-disable object-curly-newline */
 import { Request, Response } from 'express';
+import { unlink } from 'fs';
+import { promisify } from 'util';
 import ICandidate from '../interfaces/schemas/ICandidate.interface';
 import CreateCandidate from '../services/Candidate.Service';
 import UploadCV from '../services/CV.service';
+
+const unlinkFile = promisify(unlink);
 
 const createCandidate = async (req: Request, res: Response) => {
   const cv = req.file;
@@ -12,26 +16,36 @@ const createCandidate = async (req: Request, res: Response) => {
     return res.status(400).send('no cv received');
   }
 
-  const result = await UploadCV(cv);
-
-  const cvKey = result?.Key;
-
-  const data = await CreateCandidate({
-    id,
-    name,
-    email,
-    phone,
-    country,
-    cv: cvKey,
-  });
-
-  if (!data) {
-    return res.status(500).send({
-      message: 'There was an error creating the candidate. Please try again',
-    });
+  if (cv.mimetype !== 'application/pdf') {
+    await unlinkFile(cv.path);
+    return res.status(400).send({ message: 'Only PDF files are supported' });
   }
 
-  return res.status(201).send(data);
+  try {
+    const result = await UploadCV(cv);
+    await unlinkFile(cv.path);
+
+    const cvKey = result?.Key;
+
+    const data = await CreateCandidate({
+      id,
+      name,
+      email,
+      phone,
+      country,
+      cv: cvKey,
+    });
+
+    if (!data) {
+      return res.status(500).send({
+        message: 'There was an error creating the candidate. Please try again',
+      });
+    }
+
+    return res.status(201).send(data);
+  } catch (e: any) {
+    console.error(e);
+  }
 };
 
 export default createCandidate;
