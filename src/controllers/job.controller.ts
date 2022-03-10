bd9293307c14928302ab242a60e315aee1ef0ec0
Nacho Hotz <juanhotz611@gmail.com>
2022-03-10 16:44:26 -0300

    FEAT: now every job has its own url

--- src/controllers/job.controller.ts
+++ src/controllers/job.controller.ts
@@ -1,6 +1,8 @@
+/* eslint-disable no-underscore-dangle */
 import { Request, Response } from 'express';
 import IJob from '../interfaces/schemas/IJob.interface';
-import { CreateJob, SetCandidate } from '../services/Job.service';
+import * as urlService from '../services/Url.service';
+import { CreateJob, SetCandidate, SetUrl } from '../services/Job.service';
 
 export const createJob = async (req: Request, res: Response) => {
   const { title, designated }: IJob = req.body;
@@ -9,10 +11,22 @@ export const createJob = async (req: Request, res: Response) => {
     const newJob = await CreateJob({ title, designated });
 
     if (!newJob) {
-      return res.status(400).send('There was an error');
+      return res.status(500).send('There was an error');
     }
 
-    return res.status(201).send(newJob);
+    const jobUrl = await urlService.GenerateUrl();
+
+    if (!jobUrl) {
+      return res.status(500).send('There was an error');
+    }
+
+    const jobUpdated = await SetUrl(newJob._id, jobUrl.short_url);
+
+    if (!jobUpdated) {
+      return res.status(500).send('There was an error setting the url');
+    }
+
+    return res.status(201).send(jobUpdated);
   } catch (e: any) {
     return res.status(500).send(e.message);
   }
