1f8cd9b09ced19d69160b971365efb3dbd9fb85d
Nacho Hotz <juanhotz611@gmail.com>
2022-03-11 09:42:38 -0300

    FEAT: remove unused JobUrl schema. Now job urls are create in job schema. Remove candidate property

--- src/db/schemas/Job.schema.ts
+++ src/db/schemas/Job.schema.ts
@@ -1,12 +1,24 @@
+/* eslint-disable indent */
 import { Schema, model } from 'mongoose';
+import { generate } from 'shortid';
 import IJob from '../../interfaces/schemas/IJob.interface';
 
 const JobSchema = new Schema<IJob>(
   {
     title: String,
-    designated: [String],
-    candidates: [{ type: Schema.Types.ObjectId, ref: 'Candidate' }],
-    url: String,
+    designated: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    url: {
+      type: String,
+      unique: true,
+      default:
+        process.env.NODE_ENV === 'development'
+          ? `${
+              process.env.REDIRECT_URL_DEVELOPMENT
+            }/info-upload?job_id=${generate()}`
+          : `${
+              process.env.REDIRECT_URL_PRODUCTION
+            }/info-upload?job_id=${generate()}`,
+    },
   },
   { versionKey: false },
 );
