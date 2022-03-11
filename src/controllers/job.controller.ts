fd5928fa8fb721bb9db0b5013026719a25bc2342
Nacho Hotz <juanhotz611@gmail.com>
2022-03-11 10:29:33 -0300

    FIX: remove unneccesary validation

--- src/controllers/job.controller.ts
+++ src/controllers/job.controller.ts
@@ -1,8 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import { Request, Response } from 'express';
 import IJob from '../interfaces/schemas/IJob.interface';
-import * as urlService from '../services/Url.service';
-import { CreateJob, SetCandidate, SetUrl } from '../services/Job.service';
+import { CreateJob, SetCandidate } from '../services/Job.service';
 
 export const createJob = async (req: Request, res: Response) => {
   const { title, designated }: IJob = req.body;
@@ -14,19 +13,7 @@ export const createJob = async (req: Request, res: Response) => {
       return res.status(500).send('There was an error');
     }
 
-    const jobUrl = await urlService.GenerateUrl();
-
-    if (!jobUrl) {
-      return res.status(500).send('There was an error');
-    }
-
-    const jobUpdated = await SetUrl(newJob._id, jobUrl.short_url);
-
-    if (!jobUpdated) {
-      return res.status(500).send('There was an error setting the url');
-    }
-
-    return res.status(201).send(jobUpdated);
+    return res.status(201).send(newJob);
   } catch (e: any) {
     return res.status(500).send(e.message);
   }
