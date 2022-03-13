fbdeb49257fb8ba1de95af31663b3131fe038e59
Nacho Hotz <juanhotz611@gmail.com>
2022-03-13 11:30:38 -0300

    CHORE: remove unused folder views. Remove app.use config and views from utils

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,11 @@
-import express, { Application, Request, Response } from 'express';
-import path from 'path';
+import express, { Application } from 'express';
 import routes from './routes/index.routes';
 
 const app: Application = express();
 
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.json({ limit: '25mb' }));
-app.use('/public', express.static(path.join(__dirname, '../public')));
-app.use('/build', express.static(path.join(__dirname, '../build')));
-app.use('/static', express.static(path.join(__dirname, '../build/static')));
-
-app.set('view engine', 'ejs');
 
 app.use('/', routes);
-app.use('*', (_req: Request, res: Response) => {
-  res.status(404).render('pages/pageNotFound');
-});
 
 export default app;
