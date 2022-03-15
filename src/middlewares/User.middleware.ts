e26e41908e70c7af04221047614aa3dbbad678d4
Nacho Hotz <juanhotz611@gmail.com>
2022-03-15 16:12:52 -0300

    FIX: remove unused user middleware

--- src/middlewares/User.middleware.ts
+++ src/middlewares/User.middleware.ts
@@ -1,11 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import User from '../db/schemas/User.schema';
 import IUser from '../db/interfaces/User/IUser.interface';
-import Candidate from '../db/schemas/Candidate.schema';
-import ICandidate from '../db/interfaces/ICandidate.interface';
 import BadRequestException from '../exceptions/BadRequestException';
 import InternalServerException from '../exceptions/InternalServerError';
-import temp from '../lib/tempVariables';
 
 export default async function validateSignUp(
   req: Request,
@@ -28,48 +25,5 @@ export default async function validateSignUp(
         `There was an unexpected error validating the user. ${e.message}`,
       ),
     );
-  }
-}
-
-export async function validateUser(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  try {
-    const { candidateId } = req.params;
-
-    if (!candidateId) {
-      return res.status(400).send('No candidate id was received');
-    }
-
-    const candidate: ICandidate | null = await Candidate.findOne({
-      id: candidateId,
-    });
-
-    if (!candidate) {
-      return res.status(404).send({
-        status: 'failure',
-        code: 404,
-        message: `Candidate not found with id ${candidateId}`,
-      });
-    }
-
-    if (candidate?.videos_question_list?.length !== 3) {
-      const recordedVideos = candidate?.videos_question_list?.length;
-
-      return res.status(400).send({
-        status: 'failure',
-        code: 400,
-        message: `Candidate has been found, but has not recorded all their videos. Currently they have recorded ${recordedVideos} out of 3`,
-      });
-    }
-
-    temp.video_data = candidate.videos_question_list;
-    temp.cv = candidate.cv;
-
-    next();
-  } catch (e: any) {
-    console.error(e);
   }
 }
