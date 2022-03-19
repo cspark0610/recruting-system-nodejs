c8e3514976ec4737383a59161488354d22559507
Nacho Hotz <juanhotz611@gmail.com>
2022-03-19 10:19:16 -0300

    FIX: removed rules from .eslintrc.json. Add baisc test. Add coverage to .gitignore

---
+++ tests/routes/candidate.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import express, { Application } from 'express';
+import * as candidateController from '../../src/controllers/candidate.controller';
+
+const app: Application = express();
+
+describe('GET /candidate', () => {
+  test('Should return 200', () => {
+    request(app).get('/candidate').expect(200);
+  });
+
+  test('Should return an error', async () => {
+    const response = await request(app).get('/candidate').send();
+  });
+});
