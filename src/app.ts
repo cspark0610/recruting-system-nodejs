25f2dc3d0e9fa4fcafbc48fa0bec6d968af036df
Nacho Hotz <juanhotz611@gmail.com>
2022-03-19 20:42:54 -0300

    FEAT: helmet, cors, and not found endpoint page

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 import errorMiddleware from './middlewares/error.middleware';
 import createRoles from './lib/createRoles';
 import routes from './routes/index.routes';
@@ -7,6 +9,8 @@ const app: Application = express();
 
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.json({ limit: '25mb' }));
+app.use(cors());
+app.use(helmet());
 
 createRoles();
 
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -5,6 +5,7 @@ import jobRoutes from './job.routes';
 import userRoutes from './user.routes';
 
 import getKeys from '../controllers/keys.controller';
+import NotFoundException from '../exceptions/NotFoundException';
 
 const router = Router();
 
@@ -13,5 +14,9 @@ router.get('/video-key', getKeys);
 router.use('/candidate', candidateRoutes);
 router.use('/job', jobRoutes);
 router.use('/users', userRoutes);
+
+router.use('*', (_req, _res, next) =>
+  next(new NotFoundException('This page does not exists')),
+);
 
 export default router;
