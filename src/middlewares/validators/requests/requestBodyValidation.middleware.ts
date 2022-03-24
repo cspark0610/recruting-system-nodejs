8b973ab0608130393142145096d7279ac0d2cdbc
Nacho Hotz <juanhotz611@gmail.com>
2022-03-24 17:43:50 -0300

    FIX: cv file not deleting after candidate body validation

--- src/middlewares/validators/requests/requestBodyValidation.middleware.ts
+++ src/middlewares/validators/requests/requestBodyValidation.middleware.ts
@@ -1,24 +1,34 @@
 /* eslint-disable import/prefer-default-export */
 import { plainToClass } from 'class-transformer';
 import { validate, ValidationError } from 'class-validator';
+import { unlink } from 'fs';
+import { promisify } from 'util';
 import * as express from 'express';
 import BadRequestException from '../../../exceptions/BadRequestException';
 
+const unlinkFile = promisify(unlink);
+
 export function requestBodyValidation<T>(type: any): express.RequestHandler {
   return (req, _res, next) => {
-    validate(plainToClass(type, req.body)).then((errors: ValidationError[]) => {
-      if (errors.length > 0) {
-        const message = errors
-          .map((error: ValidationError) =>
-            Object.values(error.constraints as { [type: string]: string }),
-          )
-          .join(', ');
-        next(
-          new BadRequestException(message.split(',').map((err) => err.trim())),
-        );
-      } else {
-        next();
-      }
-    });
+    const cv = req.file as Express.Multer.File;
+    validate(plainToClass(type, req.body)).then(
+      async (errors: ValidationError[]) => {
+        if (errors.length > 0) {
+          await unlinkFile(cv.path);
+          const message = errors
+            .map((error: ValidationError) =>
+              Object.values(error.constraints as { [type: string]: string }),
+            )
+            .join(', ');
+          next(
+            new BadRequestException(
+              message.split(',').map((err) => err.trim()),
+            ),
+          );
+        } else {
+          next();
+        }
+      },
+    );
   };
 }
