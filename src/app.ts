0ed1dbecf674c9ebb1d722db8dc8cdd91dc8bdbc
Nacho Hotz <juanhotz611@gmail.com>
2022-03-25 13:01:49 -0300

    FEAT: morgan when node env is development

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@ import express, { Application } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import corsOptions from './config/cors';
+import envConfig from './config/env';
 import errorMiddleware from './middlewares/error.middleware';
 import createRoles from './lib/createRoles';
 import routes from './routes/index.routes';
 
 const app: Application = express();
+
+if (envConfig.NODE_ENV === 'development') {
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+}
 
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.json({ limit: '25mb' }));
