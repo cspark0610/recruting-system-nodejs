daefb00d3ab7486817be5b243d75ecb5f9e0c84b
Nacho Hotz <juanhotz611@gmail.com>
2022-04-04 10:15:21 -0300

    TASK/Candidate-status-update

---
+++ src/config/validEnums.ts
@@ -0,0 +1,14 @@
+export const valid_main_status = [
+  'interested',
+  'applying',
+  'meeting',
+  'chosen',
+];
+export const valid_secondary_status = [
+  'new entry',
+  'doubting',
+  'dismissed',
+  'aproved',
+];
+
+export const valid_user_roles = ['CEO', 'CTO', 'RRHH ADMIN', 'RRHH', 'COMMON'];
