bf77aff5602918bea8d3da7abe438dfcbef9a309
Nacho Hotz <juanhotz611@gmail.com>
2022-05-20 17:43:43 -0300

    FIX: updating candidate status logic. Add NodeJs engine

--- src/db/schemas/dtos/Candidate/UpdateConclusionsDto.dto.ts
+++ src/db/schemas/dtos/Candidate/UpdateConclusionsDto.dto.ts
@@ -3,11 +3,9 @@ import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
 export class UpdateConclusionsDto {
   @IsOptional()
   @IsString()
-  @IsNotEmpty()
   good?: string;
 
   @IsOptional()
   @IsString()
-  @IsNotEmpty()
   bad?: string;
 }
