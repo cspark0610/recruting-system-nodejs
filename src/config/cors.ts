11dfd878ad57398fdd4f75ba25114a0fdeef5f6c
Nacho Hotz <juanhotz611@gmail.com>
2022-06-14 12:30:26 -0300

    FIX: cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,7 +4,10 @@ import { envConfig } from './env';
 const { NODE_ENV } = envConfig;
 
 export const corsOptions: CorsOptions = {
-  origin: NODE_ENV === 'development' ? 'http://localhost:3000' : '*',
+  origin:
+    NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://workat-five.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
