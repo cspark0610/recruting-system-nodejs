0951b6b5068bca36c591e7aa089b49db78d01024
cspark0610 <cpark0610@gmail.com>
2022-09-22 14:28:14 -0300

    cors config ch

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Express } from "express";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { corsOptions, envConfig, swaggerDocs } from "./config";
+import { ALLOWED_ORIGINS, corsOptions, envConfig, swaggerDocs } from "./config";
 import errorMiddleware from "./middlewares/error.middleware";
 import createRoles from "./lib/createRoles";
 import routes from "./routes/index.routes";
@@ -20,6 +20,15 @@ app.use(express.json({ limit: "25mb" }));
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(helmet());
+
+app.use((req, res, next) => {
+	let origin = req.headers.origin;
+	let theOrigin = ALLOWED_ORIGINS.indexOf(origin) >= 0 ? origin : ALLOWED_ORIGINS[0];
+	res.header("Access-Control-Allow-Origin", theOrigin);
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+	next();
+});
 
 createRoles();
 swaggerDocs(app, 3001);
--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,12 +3,10 @@ import { envConfig } from "./env";
 
 const { NODE_ENV } = envConfig;
 
+export const ALLOWED_ORIGINS = ["https://workat-five.vercel.app", "http://localhost:3000"];
+
 export const corsOptions: CorsOptions = {
-	origin: [
-		"http://localhost:3000",
-		"https://workat-five.vercel.app",
-		"https://fulltimeforce-video-interview.herokuapp.com",
-	],
+	origin: ["https://workat-five.vercel.app", "http://localhost:3000"],
 	// origin: NODE_ENV === "development" ? "http://localhost:3000" : "https://fulltimeforce-video-interview.herokuapp.com/",
 	// credentials: true,
 	// methods: ["GET", "POST", "PUT", "DELETE"],
