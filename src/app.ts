de1e5ddd3d5607ad0d600a91fffc2adc12d33ac5
cspark0610 <cpark0610@gmail.com>
2022-09-22 14:38:04 -0300

    cors allow all

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Express } from "express";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { ALLOWED_ORIGINS, corsOptions, envConfig, swaggerDocs } from "./config";
+import { corsOptions, envConfig, swaggerDocs } from "./config";
 import errorMiddleware from "./middlewares/error.middleware";
 import createRoles from "./lib/createRoles";
 import routes from "./routes/index.routes";
@@ -17,18 +17,9 @@ if (envConfig.NODE_ENV === "development") {
 
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 app.use(express.json({ limit: "25mb" }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use(helmet());
-
-app.use((req, res, next) => {
-	let origin = req.headers.origin;
-	let theOrigin = ALLOWED_ORIGINS.indexOf(origin) >= 0 ? origin : ALLOWED_ORIGINS[0];
-	res.header("Access-Control-Allow-Origin", theOrigin);
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-	next();
-});
 
 createRoles();
 swaggerDocs(app, 3001);
--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,11 +3,8 @@ import { envConfig } from "./env";
 
 const { NODE_ENV } = envConfig;
 
-export const ALLOWED_ORIGINS = ["https://workat-five.vercel.app", "http://localhost:3000"];
-
 export const corsOptions: CorsOptions = {
-	origin: ["https://workat-five.vercel.app", "http://localhost:3000"],
-	// origin: NODE_ENV === "development" ? "http://localhost:3000" : "https://fulltimeforce-video-interview.herokuapp.com/",
-	// credentials: true,
-	// methods: ["GET", "POST", "PUT", "DELETE"],
+	origin: ["http://localhost:3000", "https://workat-five.vercel.app"],
+	credentials: true,
+	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
 };
